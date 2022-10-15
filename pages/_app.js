2978095803f2abf787ce595fd3139a0aaba50587
Lua Zhi Zhan <zhizhanlua@gmail.com>
2022-10-15 09:34:33 +0800

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
-import '../styles/globals.css'
+import { ThemeProvider } from "@emotion/react";
+import { createTheme } from "@mui/material/styles";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const theme = createTheme({
+    palette: {
+      type: "light",
+      primary: {
+        main: "#E91E63",
+        dark: "#C2185B",
+        light: "#F8BBD0",
+      },
+      secondary: {
+        main: "#ffc107",
+        light: "#ffecb3",
+        dark: "#ffa000",
+      },
+    },
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
